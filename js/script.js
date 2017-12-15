162314c8a70251916f7cec55209778347c1d2ec0
alima-a <flemethett@gmal.com>
2017-12-15 20:39:24 +0700

    js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,9 @@ document.querySelector('body').classList.remove('nojs');
 
 var modalTrigger = document.querySelector('.search-hotel');
 var modal = document.querySelector('.search-form');
-var date = modal.querySelector('[name=arrival-date]');
 
 modalTrigger.addEventListener('click', function(evt) {
   evt.preventDefault();
-  modal.classList.toggle('nojs')
+  modal.classList.toggle('opened')
   });
 
